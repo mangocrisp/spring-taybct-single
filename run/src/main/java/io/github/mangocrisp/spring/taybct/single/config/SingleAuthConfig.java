04552c41fce9cac0af7428d1be749bdfdc65d535
Mango Crisp <15014633363@163.com>
2025-09-16 19:21:43 +0800

    - 兼容 sqlite

--- run/src/main/java/io/github/mangocrisp/spring/taybct/single/config/SingleAuthConfig.java
+++ run/src/main/java/io/github/mangocrisp/spring/taybct/single/config/SingleAuthConfig.java
@@ -1,10 +1,16 @@
 package io.github.mangocrisp.spring.taybct.single.config;
 
 import cn.afterturn.easypoi.handler.inter.IExcelDictHandler;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.mangocrisp.spring.taybct.auth.security.filter.CaptchaFilter;
 import io.github.mangocrisp.spring.taybct.auth.security.filter.PasswordCheckFilter;
+import io.github.mangocrisp.spring.taybct.auth.security.granter.customize.CustomizeTokenAuthenticationToken;
 import io.github.mangocrisp.spring.taybct.auth.security.handle.IUserDetailsHandle;
 import io.github.mangocrisp.spring.taybct.auth.security.handle.PasswordExceptionReporter;
+import io.github.mangocrisp.spring.taybct.auth.security.pojo.OAuth2UserDetails;
+import io.github.mangocrisp.spring.taybct.auth.security.support.OAuth2SqliteAuthorizationRowMapper;
+import io.github.mangocrisp.spring.taybct.auth.security.support.OAuth2UserMixin;
 import io.github.mangocrisp.spring.taybct.auth.service.IRegisteredService;
 import io.github.mangocrisp.spring.taybct.common.prop.SecureProp;
 import io.github.mangocrisp.spring.taybct.module.system.handle.AuthServeClientHandle;
@@ -26,8 +32,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.lob.DefaultLobHandler;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationServerJackson2Module;
 
-import java.security.KeyPair;
+import java.util.List;
 
 /**
  * @author xijieyin <br> 2023/1/3 14:11
@@ -107,5 +120,35 @@ public class SingleAuthConfig {
     @Bean
     public IExcelService excelService() {
         return new ExcelServiceImpl();
+    }
+
+    /**
+     * 因为用到了 SQLite，类型转换会有问题，这里做一下类型解析器
+     * @param jdbcTemplate 数据源
+     * @param registeredClientRepository  注册
+     * @return OAuth2AuthorizationService
+     */
+    @Bean
+    public OAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate
+            , RegisteredClientRepository registeredClientRepository) {
+        JdbcOAuth2AuthorizationService service = new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
+        OAuth2SqliteAuthorizationRowMapper authorizationRowMapper =
+                new OAuth2SqliteAuthorizationRowMapper(jdbcTemplate, registeredClientRepository);
+        authorizationRowMapper.setLobHandler(new DefaultLobHandler());
+        authorizationRowMapper.setObjectMapper(objectMapper());
+        service.setAuthorizationRowMapper(authorizationRowMapper);
+        return service;
+    }
+
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ClassLoader classLoader = JdbcOAuth2AuthorizationService.class.getClassLoader();
+        List<Module> securityModules = SecurityJackson2Modules.getModules(classLoader);
+        objectMapper.registerModules(securityModules);
+        objectMapper.registerModule(new OAuth2AuthorizationServerJackson2Module());
+        //放入自定义的user类
+        objectMapper.addMixIn(OAuth2UserDetails.class, OAuth2UserMixin.class);
+        objectMapper.addMixIn(CustomizeTokenAuthenticationToken.class, OAuth2UserMixin.class);
+        return objectMapper;
     }
 }
